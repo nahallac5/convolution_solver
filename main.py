a414ef0ad7e463433a5e7f8fdd7c1d9c59157737
liamcallahan <liamcallahan5@gmail.com>
2021-02-28 18:27:09 -0500

    Finished Plots for homework, GUI planned for future use

--- main.py
+++ main.py
@@ -34,9 +34,6 @@ class Param:
 
 # Read in function for user input
 def read_in():
-    print("====================================================")
-    print("Welcome to Orp-Lith! Please enter your parameters...")
-    print("====================================================")
     accept_vars = 1
 
     # Get parameters from user
@@ -96,12 +93,11 @@ def delta_processing(ui):
 
     # Scaling setup
     u_o = 1 / ui.pitch
-    x = np.arange(-ui.pitch, ui.pitch, 0.1)
-
+    x = np.arange(-1.0 * ui.pitch, 1.0 * ui.pitch, 0.01)
     # Function builder **This for sure can be cleaner...**
     if terms == 1:
         mag_0 = ui.delta_mag_list[0]
-        m_prime = mag_0
+        m_prime = np.array([mag_0 for i in range(len(x))])
     elif terms == 2:
         mag_0 = ui.delta_mag_list[0]
         mag_1 = ui.delta_mag_list[1]
@@ -125,18 +121,66 @@ def delta_processing(ui):
         mag_4 = ui.delta_mag_list[4]
         m_prime = mag_0 + 2 * mag_1 * np.cos(2 * np.pi * u_o * x) + (2/3) * mag_2 * np.cos(6 * np.pi * u_o * x) + (2/5) * mag_3 * np.cos(10 * np.pi * u_o * x) + (2/7) * mag_4 * np.cos(14 * np.pi * u_o * x)
     else:
-        m_prime = .5
+        m_prime = np.array([.5 for i in range(len(x))])
 
     # Function plotter
     intensity = np.square(m_prime)
+
+    # Tests on intensity plot
+
+    # Image Contrast
+    image_contrast = format((max(intensity) - min(intensity)) / (max(intensity) + min(intensity)), ".3f")
+
+    # Image Log Slope
+    mask_edge = ui.space * 0.5
+    mask_edge_loc = np.searchsorted(x, mask_edge, 'left')
+    dx = np.diff(x)
+    dy = np.diff(np.log(intensity))
+    slope = dy / dx
+    ils = abs(slope[mask_edge_loc])
+
+    # Normalized Image Log Slope
+    if ui.pitch - ui.space < ui.space:
+        cd = ui.pitch - ui.space
+    else:
+        cd = ui.space
+    nils = ils * cd
+
+    # Text for figure variables
+    param_text = "Parameters:\n=======\nPitch: " + str(ui.pitch) + "nm\nSpace: " + str(ui.space) + "nm\nNA: " + str(ui.NA) + "\nλ: " + str(ui.wavelength) + "nm\n\n"
+    test_text = "Tests:\n=======\nILS: " + "{:.2e}".format(ils) + "nm$^{-1}$\nNILS: " + format(nils, ".3f") + "\nImage Cont: " + str(image_contrast)
+    out_text = param_text + test_text
+
+    # Set up figure
+    fig = plt.figure()
     plt.plot(x, intensity)
+    plt.xlabel('Arial distance from -' + str(ui.pitch) + 'nm to ' + str(ui.pitch) + 'nm')
+    plt.ylabel('Intensity')
+    plt.title('Intensity Curve')
+    fig.text(.76, 0.5, out_text, ha='left')
+    fig.set_size_inches(6, 6, forward=True)
+    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.75, top=0.9)
     plt.show()
 
 
 # Run
-ui = read_in()
-delta_filter(ui)
-delta_processing(ui)
+def run():
+    print("====================================================")
+    print("Welcome to Orp-Lith! Please enter your parameters...")
+    print("====================================================")
+    # Will keep running until told to exit
+    running = 1
+    while running != -1:
+        # Test Condition
+        # ui = Param(260, 130, 0.75, 193)
+        ui = read_in()
+        delta_filter(ui)
+        delta_processing(ui)
 
-print(ui.delta_mag_list)
-print(ui.delta_loc_list)
+        # Exit check
+        prog_loop = input("Would you like to make another chart? (Y or N): ")
+        if prog_loop.lower() in ["n", "no"]:
+            break
+
+
+run()
